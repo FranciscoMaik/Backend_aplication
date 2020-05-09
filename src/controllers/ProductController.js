c7b6976edc81fa1c8e783081b0c8a1a7bc311b31
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-09 01:02:03 -0300

    Fixing ProductController

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,21 +1,20 @@
-const database = require('../database/connection');
+const database = require("../database/connection");
 
 module.exports = {
-    async index(request, response){
-        const product = await database('product').select('*');
-        return response.json(product);
-    },
-    async create(request, response){
-        const { gtin, description, barcode_image, gpc:{ description:descrip } } = request.body;
-        const gtinString= gtin.toString();
-        await database('product').insert({
-            gtin:gtinString,
-            description,
-            barcode_image,
-            gpc_description: descrip,
-            flagCountAny: 0,
-            flagCountMonth: 0,
-        });
-        return response.json({ gtin });
-    }
-}
+  async index(request, response) {
+    const product = await database("product").select("*");
+    return response.json(product);
+  },
+  async create(request, response) {
+    const { gtin, description, barcode_image } = request.body;
+    const gtinString = gtin.toString();
+    await database("product").insert({
+      gtin: gtinString,
+      description,
+      barcode_image,
+      flagCountAny: 1,
+      flagCountMonth: 1,
+    });
+    return response.json({ gtin });
+  },
+};
