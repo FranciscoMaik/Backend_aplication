ca162713182cb90177be38ec27387148fce99fc7
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-09 00:10:33 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const routes = require("./routes");
 const app = express();
+const server = require("http").Server(app);
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
+server.listen(process.env.PORT || 3333, () => {
   console.log("Running at port 3000");
 });
