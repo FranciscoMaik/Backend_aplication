f87e9ef5de1701f4b1bf622e8bebdee736bad1ba
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-12 19:57:39 -0300

    valores

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 
+//api da empresa cosmos
 const api = axios.create({
   baseURL: "https://api.cosmos.bluesoft.com.br/gtins/",
   headers: { "x-cosmos-token": "LBWH5jeI6EzFWot32htQ1w" },
